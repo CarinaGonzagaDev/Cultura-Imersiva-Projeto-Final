fd0a21005494243f91dce2f51e29b3e0a9995566
CarinaGonzagaDev <carinamatagonzaga2006@gmail.com>
2025-09-23 11:06:17 -0300

    base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { Component } from '@angular/core';
+import { AnimeComponent } from './components/anime/anime.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+
+    }                                                                                                                                                                                                            
+];
---
+++ src/app/components/hqs/hqs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hqs',
+  imports: [],
+  templateUrl: './hqs.component.html',
+  styleUrl: './hqs.component.css'
+})
+export class HqsComponent {
+
+}
