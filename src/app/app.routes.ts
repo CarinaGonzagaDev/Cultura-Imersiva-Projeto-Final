99b725500a7df6b29e66f0b7c77f1c5a7aa3eb2a
CarinaGonzagaDev <carinamatagonzaga2006@gmail.com>
2025-10-03 09:32:33 -0300

    Rota página home funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from "./components/home/home.component";
 import { LoginComponent } from "./login/login.component";
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'cadastro', component: CadastroComponent },
