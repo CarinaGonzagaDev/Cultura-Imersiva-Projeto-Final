8a9dd17748f9b8dd0254c494f14090dfc319ee92
CarinaGonzagaDev <carinamatagonzaga2006@gmail.com>
2025-10-06 09:54:37 -0300

    Concertado erro do header.

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 
 export interface CarouselItem {
-  id: number; // <-- ADICIONADO
-  type: 'anime' | 'hq'; // <-- ADICIONADO
+  id: number;
+  type: 'anime' | 'hq';
   image: string;
   title: string;
   rating: number;
@@ -34,7 +34,21 @@ export class MediaService {
       season: 'S1', episodes: 'E12', rating: 9.8,
       genres: ['Ação', 'Fantasia Sombria', 'Horror', 'Shonen'], status: 'Finalizado'
     },
-    // ... Adicione id e type para os outros animes ...
+    { 
+      id: 4, type: 'anime', image: 'Dandadan.jpg', title: 'Dandadan', 
+      season: 'S1', episodes: 'E12', rating: 9.8,
+      genres: ['Comédia', 'Ficção Científica', 'Ação', 'Sobrenatural'], status: 'Em Lançamento'
+    },
+    { 
+      id: 5, type: 'anime', image: 'Attack On Titan.jpg', title: 'Attack on Titan', 
+      season: 'S4', episodes: 'E28', rating: 9.3,
+      genres: ['Ação', 'Suspense', 'Drama', 'Fantasia Sombria'], status: 'Finalizado'
+    },
+    {
+      id: 6, type: 'anime', image: 'SAKAMOTO DAYS.jpg', title: 'Sakamoto Days',
+      season: 'S1', episodes: 'E12', rating: 9.1,
+      genres: ['Ação', 'Comédia', 'Shonen', 'Slice of Life'], status: 'Em Lançamento'
+    }
   ];
 
   private mangas: CarouselItem[] = [
@@ -48,7 +62,21 @@ export class MediaService {
       season: 'Finalizado', episodes: '1 Cap.', rating: 9.5,
       genres: ['Drama', 'Slice of Life'], country: 'Japão (Mangá)'
     },
-    // ... Adicione id e type para os outros mangás ...
+    { 
+      id: 13, type: 'hq', image: 'Otoyomegatari Mangá.jpg', title: 'A Bride\'s Story', 
+      season: 'Em Andamento', episodes: '112 Caps.', rating: 9.7,
+      genres: ['Histórico', 'Romance', 'Slice of Life', 'Seinen'], country: 'Japão (Mangá)'
+    },
+    { 
+      id: 14, type: 'hq', image: 'Battle Angel Alita Mangá.jpg', title: 'Battle Angel Alita', 
+      season: 'Finalizado', episodes: '51 Caps.', rating: 9.3,
+      genres: ['Ação', 'Ficção Científica', 'Psicológico', 'Seinen'], country: 'Japão (Mangá)'
+    },
+    { 
+      id: 15, type: 'hq', image: 'Dragon Ball Mangá.jpg', title: 'Dragon Ball Super', 
+      season: 'Em Andamento', episodes: '104 Caps.', rating: 8.5,
+      genres: ['Ação', 'Aventura', 'Shonen'], country: 'Japão (Mangá)'
+    },
   ];
 
   constructor() { }
